bdaf2ccdd57846e7de3baea985fe7b03acb8f44f
unknown <dyshnikuflorjan@gmail.com>
2022-10-11 00:48:22 +0200

    taskweb commit

---
+++ index.js
@@ -0,0 +1,10 @@
+ $(function(){
+
+     $('.slider').slick({
+        infinite: true,
+        slidesToShow:2,
+        slidesToScroll: 2,
+        prevArrow:'.priv_arrow',
+        nextArrow:'.next_arrow'
+      });
+ } );    
